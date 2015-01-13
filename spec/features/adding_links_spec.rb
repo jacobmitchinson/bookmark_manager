2491c4fe9fe42d4ea7dccb439a62c54df9670a6d
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-01-13 18:51:21 +0000

    Added new scenario and altered #add_link. Tests currently failing.

--- spec/features/adding_links_spec.rb
+++ spec/features/adding_links_spec.rb
@@ -10,11 +10,27 @@ feature "User adds a new link" do
     expect(link.url).to eq("http://www.makersacademy.com")
   end
 
-  def add_link(url, title)
+  scenario "with a few tags" do 
+    visit "/"
+    add_link("http://www.makersacademy.com",
+                "Makers Academy",
+                ['education', 'ruby'])
+    link = Link.first
+    expect(link.tags).to include("education")
+    expect(link.tags).to include("ruby")
+  end
+
+
+  def add_link(url, title, tags = [])
     within('#new-link') do
       fill_in 'url', :with => url
       fill_in 'title', :with => title
-      click_on 'Add link'
+      # our tags will be space seperated
+      fill_in 'tags', :with => tags.join(' ')
+      click_button 'Add link'
     end
   end
+
+
+
 end
